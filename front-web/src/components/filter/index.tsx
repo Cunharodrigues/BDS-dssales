462a11c130c3eea4f3d2fc3a73ea2dba1f2580cf
cunharodrigues <pwcunha@gmail.com>
2022-08-04 16:03:09 -0300

    Add Filter component

---
+++ front-web/src/components/filter/index.tsx
@@ -0,0 +1,36 @@
+import './styles.css';
+import FlatPicker from 'react-flatpickr';
+import FlatPickerLib from 'flatpickr';
+import { Portuguese } from 'flatpickr/dist/l10n/pt';
+import 'flatpickr/dist/themes/material_green.css';
+
+FlatPickerLib.localize(Portuguese);
+
+function Filter() {
+  const onChangeDate = (dates: Date[]) => {
+    console.log(dates);
+  };
+
+  return (
+    <div className="filter-container base-card">
+      <FlatPicker
+        options={{
+          mode: 'range',
+          dateFormat: 'd/m/Y',
+          showMonths: 2
+        }}
+        className="filter-input"
+        onChange={onChangeDate}
+        placeholder="Selecione um periodo"
+      />
+      <select className="filter-input">
+        <option value="">Selecione um gênero</option>
+        <option value="Male">Masculino</option>
+        <option value="FEMALE">Feminino</option>
+        <option value="OTHER">Outros</option>
+      </select>
+    </div>
+  );
+}
+
+export default Filter;
